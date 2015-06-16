ded86776830f3e78442f2c3c455d6fb3ed451da6
cozzyy2002 <cozzyy2002@gmail.com>
2015-06-16 22:55:12 +0900

    Implemented unit test for CPacketToSend::encodeRemainingLength()

--- maquette_unittest/CPacketTest.cpp
+++ maquette_unittest/CPacketTest.cpp
@@ -22,13 +22,35 @@ public:
 	Testee testee;
 };
 
-TEST_F(PacketTest, encodeRemainingLength)
+struct _EncodedRemainingLengthTestData {
+	size_t length;		// Length to encode
+	size_t pos;			// Expected position returned by encodedRemainingLength()
+	byte encoded[4];	// Expected encoded value
+} EncodedRemainingLengthTestData[] = {
+	{0, 1, {0}},
+	{127, 1, {0x7F}},
+	{128, 2, {0x80, 0x01}},
+	{16383, 2, {0xFF, 0x7F}},
+	{16384, 3, {0x80, 0x80, 0x01}},
+	{2097151, 3, {0xFF, 0xFF, 0x7F}},
+	{2097152, 4, {0x80, 0x80, 0x80, 0x01}},
+	{268435455, 4, {0xFF, 0xFF, 0xFF, 0x7F}},
+};
+
+class EncodedRemainingLengthTest
+	: public PacketTest
+	, public WithParamInterface<size_t> {};
+
+TEST_P(EncodedRemainingLengthTest, normal)
 {
+	const _EncodedRemainingLengthTestData& data = EncodedRemainingLengthTestData[GetParam()];
+
 	byte out[4] = {0};
-	const byte expected[] = {0xff, 0xff, 0x7f, 0};
-	size_t pos = testee.encodeRemainingLength(out, 2097151);
+	size_t pos = testee.encodeRemainingLength(out, data.length);
 	for(int i = 0; i < ARRAYSIZE(out); i++) {
-		EXPECT_EQ(expected[i], out[i]) << "index=" << i;
+		EXPECT_EQ(data.encoded[i], out[i]) << "index=" << i;
 	}
-	EXPECT_EQ(3, pos);
+	EXPECT_EQ(data.pos, pos);
 }
+
+INSTANTIATE_TEST_CASE_P(CPacketToSend, EncodedRemainingLengthTest, Range((size_t)0, ARRAYSIZE(EncodedRemainingLengthTestData)));
